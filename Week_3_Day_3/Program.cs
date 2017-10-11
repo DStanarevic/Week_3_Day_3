f552302abaa1fcfb4adaaad358c0632aab3cbdc0
DStanarevic <DejanStanarevic@gmail.com>
2017-10-11 11:08:07 -0400

    Initial commit

---
+++ Week_3_Day_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_3_Day_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Inhertiance, if two classes have a lot of the same code you can include that in one base class. (Squid and Octopus in class Cephlapod.)
+            //Base Class, Class that will share it's fields and states with child classes.
+            //Members - fields, Properties and Constructors. (Anything that is inside a class.)
+            //public class ChildClass:ParentClass - This is how you would inherit the parent classes items. Each child class needs it's own constructor.
+            //Child classes do no inherit the constructors from their base classes.
+            //All items always inherit from system.object.
+
+        }
+        
+
+    }
+}
