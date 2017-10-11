faeb2bbced8cc979b3e460caf9d2569a6f22d845
DStanarevic <DejanStanarevic@gmail.com>
2017-10-11 14:20:18 -0400

    Update

--- Week_3_Day_3/Automobile.cs
+++ Week_3_Day_3/Automobile.cs
@@ -15,11 +15,21 @@ namespace Week_3_Day_3
         public int NumberOfWheels
         {
             get { return this.numberOfWheels; }
-        }    
+        }
 
-        //You use :base to update the 
-        public Automobile(int numberOfWheels, float wheelSize, int numberOfDoors):base(4,2000,"Red",65.0d)
+        //You use :base to update the fiels in the vehicle class, but not every instance will use this. public Automobile() :base(4,2000,"Red",65.0d)
+        public Automobile(int numberOfWheels, float wheelSize, int numberOfDoors, int seats, int caryingCapacity, string color, double movementSpeed)
         {
+            //These are in the Automobile class. 
+            this.numberOfWheels = numberOfWheels;
+            this.wheelSize = wheelSize;
+            this.numberOfDoors = numberOfDoors;
+            //These fields are in the vehicle class. 
+            this.seats = seats;
+            this.caryingCapacity = caryingCapacity;
+            this.color = color;
+            this.MovementSpeed = movementSpeed;
+
 
         }
 
--- Week_3_Day_3/Program.cs
+++ Week_3_Day_3/Program.cs
@@ -19,10 +19,17 @@ namespace Week_3_Day_3
             //All items always inherit from system.object.
 
             //This object is created using the parent class.
-            Vehicle baseVehicle = new Vehicle(4, 2000, "Green", 55.0d);
-            baseVehicle.Move();
-            baseVehicle.Move();
-            Console.WriteLine(baseVehicle.DistanceTraveled);
+            //Vehicle baseVehicle = new Vehicle(4, 2000, "Green", 55.0d);
+            //baseVehicle.Move();
+            //baseVehicle.Move();
+            //Console.WriteLine(baseVehicle.DistanceTraveled);
+
+            Automobile herbie = new Automobile(4,13,2,4,1500,"White",88.0d);
+            herbie.Move();
+            herbie.Move();
+            Console.WriteLine("Automobile Distance Traveled " + herbie.DistanceTraveled);
+
+
 
 
         }
